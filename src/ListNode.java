3831117771a44f285d0880a656b21c8d8f7b21ae
houkepei <17611169761@163.com>
2023-11-29 10:03:45 +0800

    2两数相加

--- src/ListNode.java
+++ src/ListNode.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ListNode {
+/**
+ * 链表
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,12 @@ public class Main {
     }
 
     /**
-     * 两数之和
+     * 1.两数之和
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+     *
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+     *
+     * 你可以按任意顺序返回答案。
      * @param nums
      * @param target
      * @return
@@ -51,6 +56,43 @@ public class Main {
             }
             return index;
         }
+
+    /**
+     * 2。两数相加
+     * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+     *
+     * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+     *
+     * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     * @param l1
+     * @param l2
+     * @return
+     */
+        public static  ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            return add(l1, l2, 0);
+        }
+
+        /**
+         返回两个链表相加的头部
+         */
+        public static  ListNode add(ListNode l1, ListNode l2, int bit) {
+            if (l1 == null && l2 == null && bit == 0) {
+                return null;
+            }
+            int val = bit;
+            if (l1 != null) {
+                val = l1.val+val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                val = l2.val+val;
+                l2 = l2.next;
+            }
+            ListNode node = new ListNode(val % 10);
+            node.next = add(l1, l2, val / 10);
+            return node;
+        }
+
 
 
 }
